44e365e67a66f49af79b98b9cba4bddb4c9495db
silent-43 <silent.alone.st@gmail.com>
2026-06-25 00:58:20 +0600

    added dynamic feature

---
+++ src/pages/Book/Book.jsx
@@ -0,0 +1,37 @@
+import React, { use } from "react";
+import { FaRegStar } from "react-icons/fa";
+
+const Book = ({ singleBook }) => {
+  // const data = use(bookPromise);
+  // console.log(data);
+//   console.log(singleBook);
+
+    const {bookName, author, image, rating, category} = singleBook;
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm border-3 p-6">
+      <figure className="p-6 bg-gray-100 w-2/3 mx-auto">
+        <img
+        className='h-[166px] w-[124px]'
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {bookName}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{category}</div>
+          <div className="badge badge-outline">{rating} <FaRegStar /></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Book;
---
+++ src/pages/Books/Books.jsx
@@ -0,0 +1,33 @@
+import React, { Suspense, useEffect, useState } from "react";
+import Book from "../Book/Book";
+
+const Books = ({ data }) => {
+  //one way to fetch
+  // const [allBooks, setAllBooks] = useState([]);
+  // useEffect(()=>{
+  //     fetch("booksData.json")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //         // console.log(data);
+  //         setAllBooks(data);
+  //     })
+  // },[])
+
+  //others way to fetch
+  // const bookPromise = fetch('./booksData.json').then(res => res.json());
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center p-6">Books</h1>
+      <Suspense fallback={<span>Loading......</span>}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+          {data.map((singleBook) => (
+            <Book key={singleBook.bookId} singleBook={singleBook}></Book>
+          ))}
+        </div>
+      </Suspense>
+    </div>
+  );
+};
+
+export default Books;
